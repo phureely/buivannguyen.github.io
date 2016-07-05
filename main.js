696b69eb65a24f0317eadd93ecbd5a15a26898b6
Bùi văn nguyện <phongcachmaytinh@gmail.com>
2016-07-06 03:38:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    $('#header ul li a').click(function(){
+    $('#header ul li a, #hire-me').click(function(){
         var targetElement = $(this).attr('href');
-        console.log(targetElement);
         $('html, body').animate({ scrollTop: $(targetElement).offset().top}, 1000);
     });
 });
